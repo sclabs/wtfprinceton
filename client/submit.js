d5ad1eefe63cb1c7ea5be9ac752928dfa171de63
Thomas Gilgenast <thomasgilgenast@gmail.com>
2013-10-27 01:47:57 -0400

    added event handling for submit form

---
+++ client/submit.js
@@ -0,0 +1,17 @@
+Template.submit.categories = function() {
+  return Categories.find({});
+}
+
+Template.submit.events({
+  "click .action-submit": function() {
+    category = $("input-category").val();
+    title = $("input-title").val();
+    text = $("input-text").val();
+    if (category && title && text) {
+      Meteor.call("submitIssue", category, title, text, function(err, result) {
+        Router.go('issue', {_id: result});
+});
+    }
+    // TODO: give feedback on the error
+  }
+});
