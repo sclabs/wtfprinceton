f67619ae969928c32f931dc5ee0c4ae9376474da
Thomas Gilgenast <thomasgilgenast@gmail.com>
2013-10-28 11:00:02 -0400

    added voting yay!

--- client/submit.js
+++ client/submit.js
@@ -4,13 +4,15 @@ Template.submit.categories = function() {
 
 Template.submit.events({
   "click .action-submit": function() {
-    category = $("input-category").val();
-    title = $("input-title").val();
-    text = $("input-text").val();
+    category = $("#input-category").val();
+    title = $("#input-title").val();
+    text = $("#input-text").val();
     if (category && title && text) {
-      Meteor.call("submitIssue", category, title, text, function(err, result) {
+      Meteor.call("submitIssue",
+                  category, title, text,
+                  function(err, result) {
         Router.go('issue', {_id: result});
-});
+      });
     }
     // TODO: give feedback on the error
   }
--- server/api.js
+++ server/api.js
@@ -1,4 +1,38 @@
-//Meteor.methods({
-//    
-//  }
-//});
+Meteor.methods({
+  submitIssue: function(category, title, text) {
+    // catch incomplete submission
+    if (!category || !title || !text)
+      return;
+    
+    // catch invalid category
+    if (Categories.find({name: category}).count() == 0)
+      return;
+    
+    // catch user not logged in
+    if (!this.userId)
+      return;
+
+    // create the Issue and return its id
+    user_netid = Meteor.user().profile.email.split('@')[0];
+    return Issues.insert({title: title,
+                          user_id: this.userId,
+                          user_netid: user_netid,
+                          category: category,
+                          text: text,
+                          timestamp: new Date()});
+  },
+
+  vote: function(issue_id, direction) {
+    if (!_.isBoolean(direction) || !this.userId)
+      return;
+    voteDoc = Votes.findOne({user: this.userId, issue: issue_id});
+    if (voteDoc)
+      Votes.update(voteDoc._id, {$set: {direction: direction,
+                                       timestamp: new Date()}});
+    else
+      Votes.insert({user: this.userId,
+                    issue: issue_id,
+                    direction: direction,
+                    timestamp: new Date()});
+  }
+});
