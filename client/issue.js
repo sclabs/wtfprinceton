6180917bf0491a0d85a4c975431bb270418f8db7
Thomas Gilgenast <thomasgilgenast@gmail.com>
2014-01-27 01:02:37 -0500

    reenabled issue editing in the ui

--- client/issue.js
+++ client/issue.js
@@ -8,16 +8,16 @@ Template.issue.owned = function() {
   return this.user_id == Meteor.userId();
 };
 
-//Template.issue.editing = function() {
-//  edit_state = Session.get('edit_state');
-//  return (edit_state &&
-//          edit_state.type == 'issue' &&
-//          edit_state.id == this._id);
-//};
+Template.issue.editing = function() {
+  edit_state = Session.get('edit_state');
+  return (edit_state &&
+          edit_state.type == 'issue' &&
+          edit_state.id == this._id);
+};
 
-//Template.issue.categories = function() {
-//  return Categories.find({});
-//}
+Template.issue.categories = function() {
+  return Categories.find({});
+}
 
 Template.issue.events({
   "click .action-upvote": function() {
@@ -28,23 +28,23 @@ Template.issue.events({
     Meteor.call('vote', this._id, false);
   },
 
-  //"click .action-edit": function() {
-  //  Session.set('edit_state', {type: 'issue', id: this._id});
-  //},
+  "click .action-edit": function() {
+    Session.set('edit_state', {type: 'issue', id: this._id});
+  },
 
-  //"click .action-cancel-edit": function() {
-  //  Session.set('edit_state', null);
-  //},
+  "click .action-cancel-edit": function() {
+    Session.set('edit_state', null);
+  },
 
-  //"click .action-submit-edit": function() {
-  //  title = $("#edit-title").val();
-  //  category = $("#edit-category").val();
-  //  text = $("#edit-text").val();
-  //  if (title && category && text) {
-  //    Meteor.call('editIssue', this._id, title, category, text);
-  //    Session.set('edit_state', null);
-  //  }
-  //},
+  "click .action-submit-edit": function() {
+    title = $("#edit-title").val();
+    category = $("#edit-category").val();
+    text = $("#edit-text").val();
+    if (title && category && text) {
+      Meteor.call('editIssue', this._id, title, category, text);
+      Session.set('edit_state', null);
+    }
+  },
 
   "click .action-confirm-delete": function() {
     $("#modal-delete-" + this._id).modal('hide');
